25fd41e6829f65c2cb67e54d6fe9c0f410fffa4f
zacmaes <zachary.maes@gmail.com>
2022-08-04 12:24:46 -0700

    Commit 1A: Added three constants; added greeting, display, and farewell strings to data section.

--- Proj5_maesz.asm
+++ Proj5_maesz.asm
@@ -6,17 +6,47 @@ TITLE Project Five: Arrays, Addressing, Stack Passed Params     (Proj5_maesz.asm
 ; Course number/section: CS271 Section 400
 ; Project Number: 5               Due Date: August 7th, 2022
 ; Description: 
-; ********WRITE DESCRIPTION HERE**********
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
+; 
 
 INCLUDE Irvine32.inc
-
 ; (insert macro definitions here)
-
 ; (insert constant definitions here)
 
-.data
+ARRAYSIZE = 200
+LO        = 15
+HI        = 50
 
+.data
 ; (insert variable definitions here)
+
+;INTRODUCTION DATA
+greeting		BYTE	"Welcome to Project Five: Arrays, Addressing, Stack Passed Params by Zachary Maes!",0
+description_1   BYTE	"This program will do something akin to magic... The programmer WILL CHANGE THIS DESCRIPTION LATER ON!!!",0
+
+
+
+; DISPLAY DATA
+unsorted_message    BYTE    "Your unsorted random numbers:",0
+median_message      BYTE    "The median value of the array: ",0
+sorted_message      BYTE    "Your sorted random numbers:",0
+list_message        BYTE    "Your list of instances of each generated number, starting with the smallest value:",0
+
+; FAREWELL DATA
+farewell_1          BYTE    "If you have made it this far, congratulations! Thanks for reading my program, goodbye!",0
 
 .code
 main PROC
