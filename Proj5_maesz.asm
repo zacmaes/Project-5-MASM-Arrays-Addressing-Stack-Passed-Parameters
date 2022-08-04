017b8d974cd51abad42356e379c6491cfc28ed93
zacmaes <zachary.maes@gmail.com>
2022-08-04 14:47:26 -0700

    Commit 1B: Added more description.

--- Proj5_maesz.asm
+++ Proj5_maesz.asm
@@ -6,21 +6,76 @@ TITLE Project Five: Arrays, Addressing, Stack Passed Params     (Proj5_maesz.asm
 ; Course number/section: CS271 Section 400
 ; Project Number: 5               Due Date: August 7th, 2022
 ; Description: 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
-; 
+
+
+; PROGRAM DESCRIPTION FROM CANVAS
+
+; 1. Introduce the program
+
+; 2. Declare global constants with initial sizes ( ARRAYSIZE=200, LO=15, and HI=50.)
+;		-Generate ARRAYSIZE random integers in the range from LO to HI (Inclusive)
+;		-Store these random integers in consecutive elements of array randArray.
+;		-Hint: Call Randomize once in main to generate a random seed. Later, use RandomRange to generate each random number.
+;					HI - (LO - 1) into EAX to get EAX, 36... returned random in range [0 - 35 inc.]...add LO [15] to the random val in EAX when done.
+
+; 3. Display the list of integers before sorting, 20 numbers per line with one space between eah value.
+
+; 4. Sort the list in ascending order (smallest first)
+
+; 5. Calculate and display the median value of the sorted randArray, rounded to the nearest integer. (USING Round half up rounding described in Canvas) 
+
+; 6. Display the sorted randArray, 20 numbers per line with one space between each value. Use (not a complete sentance in canvas...)
+
+; 7. generate an array counts which holds the number of times each value in the range [LO, HI] is ssen in randArray, 
+;		even if the number of times a value is seen is zero.
+;	    For example check canvas...also report 0 count as a 0 in the array
+
+; 8. Display the array counts, 20 numbers per line with one space between each value.
+
+
+
+; PROGRAM REQUIREMENTS FROM CANVAS
+
+; 1. The program must be constructed using procedures. Required procedures listed below:
+;		a. main
+;			input parameters: N/A
+;		   output parameters: N/A
+
+;		b. introduction
+;			input parameters: intro1 (reference), intro2 (reference), ...etc if needed
+;		   output parameters: N/A
+
+;       c. fillArray
+;			input parameters: N/A
+;		   output parameters: someArray (reference)
+;						Note: LO, HI, ARRAYSIZE will be used as globals within this procedure
+
+;		d. sortList
+;			input parameters: 
+;		   output parameters:
+
+;		e. exchangeElements
+;			input parameters:
+;		   output parameters:
+
+;		f. displayMedian
+;			input parameters:
+;		   output parameters:
+
+;		g. displayList
+;			input parameters:
+;		   output parameters:
+
+;		h. countList
+;			input parameters:
+;		   output parameters:
+;
+;
+;
+;
+;
+;
+;
 
 INCLUDE Irvine32.inc
 ; (insert macro definitions here)
@@ -52,6 +107,7 @@ farewell_1          BYTE    "If you have made it this far, congratulations! Than
 main PROC
 
 ; (insert executable instructions here)
+
 
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
